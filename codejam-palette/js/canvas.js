25fb21dd2dbf0c6fbdf2769bdfdfb4846a8ad60b
truhin-andrei <truhin-andrei@bk.ru>
2019-11-10 16:19:30 +0700

    feat: add pencil

--- codejam-palette/js/canvas.js
+++ codejam-palette/js/canvas.js
@@ -10,7 +10,7 @@ const sample = document.getElementById('sample');
 
 let activeTool;
 let toolsEvent = panelTools.addEventListener('click', tools);
-var eventClick = new MouseEvent('click', {
+let eventClick = new MouseEvent('click', {
   'view': window,
   'bubbles': true,
   'cancelable': true
@@ -19,11 +19,9 @@ var eventClick = new MouseEvent('click', {
 // предустановка сохранёных значений после перезагрузки
 if(localStorage.getItem('tool')){
   activeTool = document.getElementById(localStorage.getItem('tool'));
-  activeTool.classList.add('panel-tools--active');
   activeTool.dispatchEvent(eventClick);
 } else {
   activeTool = document.getElementById('bucket');
-  activeTool.classList.add('panel-tools--active');
   activeTool.dispatchEvent(eventClick);
   localStorage.setItem('tool', 'bucket');
 }
@@ -33,30 +31,80 @@ document.body.style.setProperty("--prevColor", localStorage.getItem('prevColor')
 document.body.style.setProperty("--color", localStorage.getItem('color'));
 }
 
- // инструменты
- function bucket() { 
-  context.fillStyle = color;
-  context.fillRect(0, 0, 512, 512);
+if(localStorage.getItem('imgType')){
+  activeTool = document.getElementById(localStorage.getItem('imgType'));
+  activeTool.classList.add('panel-tools--active');
+  //activeTool.dispatchEvent(eventClick);
+} else {
+  activeTool = document.getElementById('four');
+  //activeTool.dispatchEvent(eventClick);
+  activeTool.classList.add('panel-tools--active');
+  localStorage.setItem('imgType', 'four');
 }
 
-function tools(event){
+
+
+function draw(event) {
+  let x = event.layerX;
+  let y = event.layerY;
+  let pixelX = localStorage.getItem('pixel.x');
+  let pixelY =  localStorage.getItem('pixel.y');
+  x = Math.ceil(x / pixelX) * pixelX - pixelX;
+  y = Math.ceil(y / pixelY) * pixelY - pixelY;
+  context.fillStyle = localStorage.getItem("color");
+  context.fillRect(x, y, pixelX, pixelY);
+}
+
+function pencilDown(event){
+  
+    canvas.addEventListener("mousemove", draw);
+  
+}
+function pencilUp(event){
+
+    canvas.removeEventListener("mousemove", draw);
+  
+}
+
+
+ // инструменты
+
+ function tools(event){
   if(localStorage.getItem('tool')){
     document.getElementById(localStorage.getItem('tool')).classList.remove('panel-tools--active');
+
     if (localStorage.getItem('tool') === 'bucket'){
       canvas.removeEventListener('click', bucket);
     } else if (localStorage.getItem('tool') === 'picker'){
       canvas.removeEventListener('mousemove', picker);
+      canvas.removeEventListener('click', swapColor);
+    }else if (localStorage.getItem('tool') === 'pencil'){
+      canvas.removeEventListener('mousedown', pencilDown);
+      canvas.removeEventListener('mouseup', pencilUp);
+      canvas.removeEventListener("mousemove", draw);
     }
   }
   localStorage.setItem('tool', event.target.id);
   event.target.classList.add('panel-tools--active');
+
   if (localStorage.getItem('tool') === 'bucket'){
     canvas.addEventListener('click', bucket);
   } else if (localStorage.getItem('tool') === 'picker'){
     canvas.addEventListener('mousemove', picker);
     canvas.addEventListener('click', swapColor);
+  } else if (localStorage.getItem('tool') === 'pencil'){
+    
+canvas.addEventListener("mousedown",  pencilDown);
+canvas.addEventListener("mouseup",  pencilUp);
   }
 }
+
+ function bucket() { 
+  context.fillStyle = color;
+  context.fillRect(0, 0, 512, 512);
+}
+
+
 
 function picker(event){
       let sampleData = context.getImageData(event.layerX, event.layerY, 1, 1);
@@ -93,6 +141,11 @@ four.addEventListener('click', () => fetch('./assets/json/4x4.json')
     let dy = 0;
     const width = canvas.width / matrix[0].length;
     const height = canvas.height / matrix.length;
+    localStorage.setItem('pixel.x', width);
+    localStorage.setItem('pixel.y', height);
+    document.getElementById(localStorage.getItem('imgType')).classList.remove('panel-tools--active');
+    localStorage.setItem('imgType', 'four');
+    four.classList.add('panel-tools--active');
     matrix.forEach((row) => {
       if (dx !== 0) dy += height;
       dx = 0;
@@ -111,6 +164,11 @@ thirtyTwo.addEventListener('click', () => fetch('./assets/json/32x32.json')
     let dy = 0;
     const width = canvas.width / matrix[0].length;
     const height = canvas.height / matrix.length;
+    localStorage.setItem('pixel.x', width);
+    localStorage.setItem('pixel.y', height);
+    document.getElementById(localStorage.getItem('imgType')).classList.remove('panel-tools--active');
+    localStorage.setItem('imgType', 'thirty_two');
+    thirtyTwo.classList.add('panel-tools--active');
     matrix.forEach((row) => {
       if (dx !== 0) dy += height;
       dx = 0;
@@ -126,6 +184,11 @@ picture.addEventListener('click', () => fetch('./assets/img/image.png')
   .then((response) => response.blob())
   .then(createImageBitmap)
   .then((image) => {
+    localStorage.setItem('pixel.x', 1);
+    localStorage.setItem('pixel.y', 1);
+    document.getElementById(localStorage.getItem('imgType')).classList.remove('panel-tools--active');
+    localStorage.setItem('imgType', 'picture');
+    picture.classList.add('panel-tools--active');
     context.drawImage(image, 0, 0, canvas.width, canvas.height);
   }));
 
