eca1e2b88a781fe01b6532a8fab9f5328ab79496
truhin-andrei <truhin-andrei@bk.ru>
2019-12-30 20:57:46 +0700

    refactor: readjust webpack

--- simple-piskel-clone/webpack.config.js
+++ simple-piskel-clone/webpack.config.js
@@ -1,28 +1,46 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const path = require('path');// update from 23.12.2018
+//const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: "./src/app.js",
+  entry: { main: './src/index.js' },
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "index-bundle.js"
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   },
+  target: 'node', // update from 23.12.2018
+  //externals: [nodeExternals()], // update from 23.12.2018
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: {
+          loader: "babel-loader"
+        }
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.scss$/,
+        use:  
+        [  
+          'style-loader',
+         MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          "sass-loader"
+        ]
       }
     ]
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
     })
   ]
 };
