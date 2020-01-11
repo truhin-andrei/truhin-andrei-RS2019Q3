c3b8cb342a5eb57e8b4969c96a9adc99750a89c4
truhin-andrei <truhin-andrei@bk.ru>
2020-01-11 18:38:31 +0700

    feat: add frame list

--- simple-piskel-clone/src/index.js
+++ simple-piskel-clone/src/index.js
@@ -2,12 +2,14 @@ import './style.scss';
 import './screens/header/style.scss';
 import './screens/canvas/canvas.scss';
 import './screens/tools/tools.scss';
+import './screens/frames/frames.scss';
 import './screens/canvasSize/canvasSize.scss';
 
 import './components/init/init.js';
 import './screens/canvas/canvas.js';
 import './screens/colorPanel/colorPanel';
 import './screens/tools/tools.js';
+import './screens/frames/frames.js';
 import './screens/canvasSize/canvasSize';
 
 
---
+++ simple-piskel-clone/src/screens/frames/frames.js
@@ -0,0 +1,17 @@
+import {canvas, context} from '../canvas/canvas';
+const framesList = document.getElementById('framesList');
+const addFrameBtn = document.getElementById('addFrameBtn');
+
+function addFrame() {
+  const img = new Image();
+  img.src = canvas.toDataURL();
+  
+  img.setAttribute('width', localStorage.getItem('canvasSize'));
+  img.setAttribute('height', localStorage.getItem('canvasSize'));
+  let div = document.createElement('div');
+  div.classList.add('frames__frame');
+  div.append(img);
+  framesList.insertAdjacentElement('beforeend', div);
+}
+
+addFrameBtn.addEventListener('click', addFrame);
