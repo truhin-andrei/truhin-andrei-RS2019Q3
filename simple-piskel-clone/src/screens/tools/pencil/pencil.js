1b4da7b895421756fee2075bbf6f949df7d4c30b
truhin-andrei <truhin-andrei@bk.ru>
2020-01-12 19:33:29 +0700

    feat: add pencil size option

--- simple-piskel-clone/src/screens/tools/pencil/pencil.js
+++ simple-piskel-clone/src/screens/tools/pencil/pencil.js
@@ -1,16 +1,21 @@
 import {canvas, context} from '../../canvas/canvas';
 
+const pencilSize = document.getElementById('pencilSize');
+
+function getPencilSize(){
+  return pencilSize.value;
+}
+
 function draw(event) {
   let x = event.layerX;
   let y = event.layerY;
   const canvasSize = localStorage.getItem('canvasSize');
   const realCanvasSize = 512;
   const canvasRatio = realCanvasSize/canvasSize;
-// todo set canvasSize 
   x = Math.ceil(x / canvasRatio) * canvasRatio - canvasRatio;
   y = Math.ceil(y / canvasRatio) * canvasRatio - canvasRatio;
   context.fillStyle = localStorage.getItem('color');
-  context.fillRect(x, y, canvasRatio, canvasRatio);
+  context.fillRect(x, y, canvasRatio * getPencilSize(), canvasRatio * getPencilSize());
 }
 
 export function pencilDown() {
--- simple-piskel-clone/src/screens/tools/tools.js
+++ simple-piskel-clone/src/screens/tools/tools.js
@@ -8,33 +8,35 @@ import {swapColor, colorSwitch} from './../colorPanel/colorPanel';
 const panelTools = document.getElementById('tools');
 
 function tools(event) {
-
-  if (localStorage.getItem('tool')) {
-    document.getElementById(localStorage.getItem('tool')).classList.remove('panel-tools--active');
-    if (localStorage.getItem('tool') === 'bucket') {
-      canvas.removeEventListener('click', bucket);
-    } else if (localStorage.getItem('tool') === 'picker') {
-      canvas.removeEventListener('mousemove', picker);
-      canvas.removeEventListener('click', swapColor);
-    } else if (localStorage.getItem('tool') === 'pencil') {
-      canvas.removeEventListener('mousedown', pencilDown);
-      canvas.removeEventListener('mouseup', pencilUp);
+  if(event.target.id !== 'pencilSize'){
+    if (localStorage.getItem('tool')) {
+      document.getElementById(localStorage.getItem('tool')).classList.remove('panel-tools--active');
+      if (localStorage.getItem('tool') === 'bucket') {
+        canvas.removeEventListener('click', bucket);
+      } else if (localStorage.getItem('tool') === 'picker') {
+        canvas.removeEventListener('mousemove', picker);
+        canvas.removeEventListener('click', swapColor);
+      } else if (localStorage.getItem('tool') === 'pencil') {
+        canvas.removeEventListener('mousedown', pencilDown);
+        canvas.removeEventListener('mouseup', pencilUp);
+    
+      }
+    }
+    localStorage.setItem('tool', event.target.id);
+    event.target.classList.add('panel-tools--active');
   
+    if (localStorage.getItem('tool') === 'bucket') {
+      canvas.addEventListener('click', bucket);
+    } else if (localStorage.getItem('tool') === 'picker') {
+      canvas.addEventListener('mousemove', picker);
+      canvas.addEventListener('click', swapColor);
+    } else if (localStorage.getItem('tool') === 'pencil') {
+      canvas.addEventListener('mousedown', pencilDown);
+      canvas.addEventListener('mouseup', pencilUp);
     }
   }
-  localStorage.setItem('tool', event.target.id);
-  event.target.classList.add('panel-tools--active');
 
-
-  if (localStorage.getItem('tool') === 'bucket') {
-    canvas.addEventListener('click', bucket);
-  } else if (localStorage.getItem('tool') === 'picker') {
-    canvas.addEventListener('mousemove', picker);
-    canvas.addEventListener('click', swapColor);
-  } else if (localStorage.getItem('tool') === 'pencil') {
-    canvas.addEventListener('mousedown', pencilDown);
-    canvas.addEventListener('mouseup', pencilUp);
-  }
+  
 }
 
 panelTools.addEventListener('click', tools);
